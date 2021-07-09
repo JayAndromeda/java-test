821fff12c8d721082ba5f84726920e673bade680
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 17:54:55 -0400

    covert array of items to an arrayList

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,40 +1,47 @@
+import data.Cart;
 import data.Item;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Shop {
     public static void main(String[] args) {
         printHeader();
-        Item[] items = initItems();
+        List<Item> items = initItems();
         printItems(items);
-        shopping();
+        shopping(items);
     }
 
-    private static Item[] initItems() {
-        return new Item[]{
+    private static List<Item> initItems() {
+        Item[] items =  new Item[]{
             new Item("soup", "can", 0.65f),
             new Item("bread", "loaf", 0.80f),
             new Item("milk", "bottle", 1.30f),
             new Item("apples", "single", 0.10f)
         };
+        return new ArrayList<Item>(Arrays.asList(items));
     };
 
     private static void printHeader() {
         System.out.println("Welcome to Henry's Shop");
     }
-    private static void printItems(Item[] items) {
+    private static void printItems(List<Item> items) {
         for (Item item: items) {
             System.out.println(item.toString());
         }
     }
 
-    private static void shopping() {
+    private static void shopping(List<Item> items) {
         boolean isShopping = true;
+        Cart cart = new Cart();
         while(isShopping) {
             System.out.println("To add an item simply type its name and hit enter");
             System.out.println("To stop shopping simply type exit and hit enter");
             Scanner scanner = new Scanner(System.in);
-            if (scanner.nextLine().equals("exit")) {
+            String inputName = scanner.nextLine();
+            if (inputName.equals("exit")) {
                 isShopping = false;
             }
         }
