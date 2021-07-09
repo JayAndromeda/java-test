a3ecfd621200fa94d266eb55a21a1ed59f94c0a1
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 18:58:27 -0400

    adjust test case for quantity field

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,9 +1,11 @@
 import data.Item;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ShopTest {
+
     @Test
     public void canary() {
         assertTrue(true);
@@ -11,7 +13,7 @@ public class ShopTest {
 
     @Test
     public void printItemToString() {
-        Item item = new Item("apples", "single", 0.85f);
+        Item item = new Item("apples", "single", 0.85f, 0);
         String output = item.toString();
         assertEquals("apples cost 0.85 per single", output);
     }
