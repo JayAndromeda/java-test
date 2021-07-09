17854094e4cd92f9bb6a75a8f8e59570f07edee2
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 15:39:06 -0400

    initialize data Item data class

---
+++ src/main/java/data/Item.java
@@ -0,0 +1,13 @@
+package data;
+
+public class Item {
+    private final String name;
+    private final String unit;
+    private final float cost;
+
+    public Item(String name, String unit, float cost) {
+        this.name = name;
+        this.unit = unit;
+        this.cost = cost;
+    }
+}
