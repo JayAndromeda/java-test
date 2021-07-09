8a0a2e1c835aa2d6bb42d43b7ecbb007634ddf6d
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 16:04:37 -0400

    add items to the shop

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,5 +1,22 @@
+import data.Item;
+
 public class Shop {
     public static void main(String[] args) {
-        System.out.println("init");
+        printHeader();
+        Item[] items = initItems();
+    }
+
+    private static Item[] initItems() {
+
+        return new Item[]{
+                new Item("soup", "can", 0.65f),
+                new Item("bread", "loaf", 0.80f),
+                new Item("milk", "bottle", 1.30f),
+                new Item("apples", "single", 0.10f)
+        };
+    };
+
+    private static void printHeader() {
+        System.out.println("Welcome to Henry's Shop");
     }
 }
