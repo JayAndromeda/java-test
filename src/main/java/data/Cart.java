03c5f4090f47cbc852acb01f1b248e5b3eeb3f47
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 19:30:51 -0400

    add logic to display the cart

--- src/main/java/data/Cart.java
+++ src/main/java/data/Cart.java
@@ -1,7 +1,5 @@
 package data;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.ArrayList;
 
 public class Cart {
@@ -13,5 +11,12 @@ public class Cart {
 
     public void addItem(Item item) {
         this.cart.add(item);
+    }
+
+    public void showCart() {
+        System.out.print("Your cart: ");
+        for (Item item : this.cart) {
+            System.out.print(item.getQuantity() + " " + item.getName() + " ");
+        }
     }
 }
