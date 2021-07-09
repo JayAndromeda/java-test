7d5f5c223e6cd1afaa349443619c2ae616c15f98
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 16:19:57 -0400

    add logic to Item class to retrieve info

--- src/main/java/data/Item.java
+++ src/main/java/data/Item.java
@@ -10,4 +10,21 @@ public class Item {
         this.unit = unit;
         this.cost = cost;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s cost %s per %s", getName(),getCost(),getUnit());
+    }
 }
