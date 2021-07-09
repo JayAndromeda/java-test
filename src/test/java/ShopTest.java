bd43a805a9d3f6da25ffcbdf4a7bb71a47b9bda3
JayAndromeda <nnja.jaay@gmail.com>
2021-07-09 16:19:16 -0400

    add test case to print Item info

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,3 +1,4 @@
+import data.Item;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,5 +7,12 @@ public class ShopTest {
     @Test
     public void canary() {
         assertTrue(true);
+    }
+
+    @Test
+    public void printItemToString() {
+        Item item = new Item("apples", "single", 0.85f);
+        String output = item.toString();
+        assertEquals("apples cost 0.85 per single", output);
     }
 }
